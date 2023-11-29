5d8f5e8ec8c6c9eef4fba1c490ae913c4003ff51
Peyman Bayat <p2436b@gmail.com>
2023-11-29 17:54:16 +0300

    Add some functionalities.

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -2,76 +2,131 @@
 
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Marketplace is ReentrancyGuard {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _itemIds;
-    Counters.Counter private _itemsSold;
-
-    address public owner;
-
-    struct MarketItem {
-        uint itemId;
-        uint tokenId;
-        uint price;
-        address nftContract;
-        address payable seller;
-        address payable owner;
-        bool sold;
+contract MulkiyetMarket is ERC721, Ownable {
+    struct SharedOwner {
+        address owner;
+        uint256 sharePercentage;
     }
 
-    event MarketItemCreated(
-        uint indexed itemId,
-        uint indexed tokenId,
-        uint price,
-        address indexed nftContract,
-        address seller,
-        address owner,
-        bool sold
+    struct Token {
+        SharedOwner[] sharedOwners;
+        mapping(address => uint256) ownerToIndex;
+    }
+
+    mapping(uint256 => Token) private tokens;
+
+    event SharedOwnershipAdded(
+        uint256 indexed tokenId,
+        address indexed newOwner,
+        uint256 sharePercentage
+    );
+    event SharedOwnershipRemoved(
+        uint256 indexed tokenId,
+        address indexed removedOwner
     );
 
-    mapping(uint => MarketItem) private idToMarketItem;
+    constructor() ERC721("Mulkiyet Market", "MM") Ownable() {}
 
-    constructor() {
-        owner == msg.sender;
+    function mint(
+        address[] memory _owners,
+        uint256[] memory _percentages,
+        uint256 _tokenId
+    ) external onlyOwner {
+        require(
+            _owners.length == _percentages.length,
+            "Array lengths don't match"
+        );
+
+        Token storage token = tokens[_tokenId];
+        uint256 totalPercentage;
+
+        for (uint256 i = 0; i < _owners.length; i++) {
+            require(_owners[i] != address(0), "Invalid owner address");
+            require(
+                _percentages[i] > 0 && _percentages[i] <= 100,
+                "Invalid percentage"
+            );
+
+            token.sharedOwners.push(SharedOwner(_owners[i], _percentages[i]));
+            token.ownerToIndex[_owners[i]] = i;
+            totalPercentage += _percentages[i];
+        }
+
+        require(totalPercentage == 100, "Total percentage must be 100");
+
+        _safeMint(msg.sender, _tokenId);
     }
 
-    function createMarketItem(
-        address nftContract,
-        uint tokenId,
-        uint price
-    ) public payable nonReentrant {
-        require(price > 0, "Price must be greater than 0");
+    function addSharedOwner(
+        uint256 _tokenId,
+        address _newOwner,
+        uint256 _newPercentage
+    ) external onlyOwner {
+        Token storage token = tokens[_tokenId];
+        require(token.ownerToIndex[_newOwner] == 0, "Owner already exists");
 
-        _itemIds.increment();
-        uint itemId = _itemIds.current();
+        token.sharedOwners.push(SharedOwner(_newOwner, _newPercentage));
+        token.ownerToIndex[_newOwner] = token.sharedOwners.length;
 
-        idToMarketItem[itemId] = MarketItem(
-            itemId,
-            tokenId,
-            price,
-            nftContract,
-            payable(msg.sender),
-            payable(address(0)),
-            false
+        emit SharedOwnershipAdded(_tokenId, _newOwner, _newPercentage);
+    }
+
+    function removeSharedOwner(
+        uint256 _tokenId,
+        address _ownerToRemove
+    ) external onlyOwner {
+        Token storage token = tokens[_tokenId];
+        require(
+            token.ownerToIndex[_ownerToRemove] != 0,
+            "Owner does not exist"
         );
 
-        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+        uint256 index = token.ownerToIndex[_ownerToRemove] - 1;
+        delete token.sharedOwners[index];
+        token.ownerToIndex[_ownerToRemove] = 0;
 
-        emit MarketItemCreated(
-            itemId,
-            tokenId,
-            price,
-            nftContract,
-            msg.sender,
-            address(0),
-            false
+        emit SharedOwnershipRemoved(_tokenId, _ownerToRemove);
+    }
+
+    function getTokenSharedOwners(
+        uint256 _tokenId
+    ) external view returns (SharedOwner[] memory) {
+        return tokens[_tokenId].sharedOwners;
+    }
+
+    function transferShare(
+        uint256 _tokenId,
+        address _from,
+        address _to,
+        uint256 _percentage
+    ) external onlyOwner {
+        require(_from != _to, "Cannot transfer share to the same owner");
+        require(_percentage > 0 && _percentage <= 100, "Invalid percentage");
+
+        Token storage token = tokens[_tokenId];
+        require(token.ownerToIndex[_from] != 0, "Sender owner does not exist");
+        require(token.ownerToIndex[_to] == 0, "Recipient owner already exists");
+
+        uint256 fromIndex = token.ownerToIndex[_from] - 1;
+        require(
+            token.sharedOwners[fromIndex].sharePercentage >= _percentage,
+            "Insufficient share to transfer"
         );
+
+        token.sharedOwners[fromIndex].sharePercentage -= _percentage;
+
+        if (token.sharedOwners[fromIndex].sharePercentage == 0) {
+            delete token.sharedOwners[fromIndex];
+            token.ownerToIndex[_from] = 0;
+        }
+
+        token.sharedOwners.push(SharedOwner(_to, _percentage));
+        token.ownerToIndex[_to] = token.sharedOwners.length;
+
+        emit SharedOwnershipAdded(_tokenId, _to, _percentage);
+        emit SharedOwnershipRemoved(_tokenId, _from);
     }
 }
